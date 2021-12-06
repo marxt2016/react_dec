64533b0b94745c6504aa6dfb10b6554c0e5fb163
Olga Melnikova <omelnikova@accesssoftek.com>
2021-12-06 20:30:15 +0400

    lesson2 hw

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,58 +1,45 @@
-import React, { Fragment, useState } from "react";
+import React from "react";
 
-export const Counter = () => {
-  const [count, setCount] = useState(0);
-  const [tags, setTags] = useState(["tag1", "tag2", "tag3"]);
+export const Counter = (props) => {
+  const { value } = props;
 
-  const formatCount = () => {
-    return count === 0 ? <h1>empty</h1> : count;
+  const formatValue = () => {
+    return value === 0 ? "empty" : value;
   };
 
   const getBageClasses = () => {
-    let classes = "badge m-2 ";
-    classes += count === 0 ? "bg-warning" : "bg-primary";
+    let classes = "badge m-1 ";
+    classes += value === 0 ? "bg-warning" : "bg-primary";
     return classes;
   };
 
   const handleIncrement = () => {
-    setCount((prevState) => prevState + 1);
+    props.onIncrement();
   };
 
   const handleDecrement = () => {
-    setCount((prevState) => prevState - 1);
+    props.onDecrement();
   };
-  const handleTagChange = (id) => {
-    setTags((prevState) => prevState.filter((tag) => tag !== id));
-  };
-
-  const renderTags = () => {
-    return (
-      tags.length !== 0 &&
-      tags.map((tag) => (
-        <li
-          key={tag}
-          className="btn btn-primary btn-sm m-2"
-          onClick={() => handleTagChange(tag)}
-        >
-          {tag}
-        </li>
-      ))
-    );
-  };
-
-  if (tags.length !== 0) {
-    return <ul>{renderTags()}</ul>;
-  }
 
   return (
-    <Fragment>
-      <span className={getBageClasses()}>{formatCount()}</span>
+    <div>
+      <span>{props.name}</span>
+
+      <span className={getBageClasses()}>{formatValue()}</span>
+
       <button className="btn btn-primary btn-sm m-2" onClick={handleIncrement}>
         +
       </button>
       <button className="btn btn-primary btn-sm m-2" onClick={handleDecrement}>
         -
       </button>
-    </Fragment>
+      <button
+        className="btn btn-danger btn-sm m-2"
+        onClick={() => props.onDelete(props.id)}
+      >
+        {" "}
+        delete
+      </button>
+    </div>
   );
 };
