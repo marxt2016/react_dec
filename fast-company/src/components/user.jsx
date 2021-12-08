6160a3fa7d0ddb10fdfa85967a74f86c1c706c6d
Olga Melnikova <omelnikova@accesssoftek.com>
2021-12-08 18:54:03 +0400

    initial HW2 commit

---
+++ fast-company/src/components/user.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Qualitie from "./qualitie";
+import Bookmark from "./bookmark";
+const User = () => {};
+
+export default User;
--- fast-company/src/index.js
+++ fast-company/src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
-import Users from "./components/users";
+
 import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Users />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
