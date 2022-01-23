8e16fb7cde3a10f2e94eb66544fc9db194fe8ec4
Olga Melnikova <omelnikova@accesssoftek.com>
2022-01-23 12:01:37 +0400

    UseCallBack

--- src/app/components/examples/useCallback/useCallBackExample.jsx
+++ src/app/components/examples/useCallback/useCallBackExample.jsx
@@ -1,11 +1,52 @@
-import React from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import CardWrapper from "../../common/Card";
 import SmallTitle from "../../common/typografy/smallTitle";
+import Divider from "../../common/divider";
 
 const UseCallBackExample = () => {
+    const [data, setData] = useState({});
+    const woCallback = useRef(0);
+    const withCallback = useRef(0);
+    const handleChange = ({ target }) => {
+        setData((prevState) => ({
+            ...prevState,
+            [target.name]: [target.value]
+        }));
+    };
+    const validateWoCallback = (data) => {
+        console.log(data);
+    };
+    const validateWithCallback = useCallback((data) => {
+        console.log(data);
+    }, []);
+    useEffect(() => {
+        validateWoCallback(data);
+        validateWithCallback(data);
+    }, [data]);
+    useEffect(() => {
+        woCallback.current++;
+    }, [validateWoCallback]);
+
+    useEffect(() => {
+        withCallback.current++;
+    }, [validateWithCallback]);
     return (
         <CardWrapper>
             <SmallTitle>Example</SmallTitle>
+            <Divider />
+            <p>Render woCallback: {woCallback.current}</p>
+            <p>Render withCallback: {withCallback.current}</p>
+            <label htmlFor="email" className="form-label">
+                Email
+            </label>
+            <input
+                onChange={handleChange}
+                value={data.email || ""}
+                type="email"
+                name="email"
+                className="form-control"
+                id="email"
+            />
         </CardWrapper>
     );
 };
