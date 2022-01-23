3c12e01a1d1e8dc26a90e1412093a8c75cae44e8
Olga Melnikova <omelnikova@accesssoftek.com>
2022-01-23 08:39:30 +0400

    hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import App from "./app";
+import App from "./app/App";
+import { BrowserRouter } from "react-router-dom";
 
-reactDom.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
