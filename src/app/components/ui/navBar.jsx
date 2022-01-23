456495e07e87910bd95f3bf42b9c3c0a0b3f517b
Olga Melnikova <omelnikova@accesssoftek.com>
2022-01-23 08:56:30 +0400

    test commit for hooks

--- src/app/components/ui/navBar.jsx
+++ src/app/components/ui/navBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+
 const NavBar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
