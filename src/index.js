c8461f51512aeed3e0911296d6d7fc7d29f9c1ff
Olga Melnikova <omelnikova@accesssoftek.com>
2022-02-22 09:15:53 +0400

    pipe and compose from lodash

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { compose, pipe } from "lodash/fp";
 
 const App = (params) => {
-  return <h1>App</h1>;
+  const x = 2;
+
+  const double = (number) => number * 2;
+  const square = (number) => number * number;
+  const half = (number) => number / 2;
+
+  const mathCalc = compose(half, square, double); // all functions for compose are executed in reverse order
+  const mathCalcPipe = pipe(double, square, half); // pipe executes in direct order
+
+  return <h1>{mathCalcPipe(x)}</h1>;
 };
 
 ReactDOM.render(
