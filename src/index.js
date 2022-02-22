949fb0793f460e6251f5eeda2c5015b80b7f3f60
Olga Melnikova <omelnikova@accesssoftek.com>
2022-02-22 10:13:46 +0400

    currying

--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ const App = (params) => {
   const double = (number) => number * 2;
   const square = (number) => number * number;
   const half = (number) => number / 2;
-
-  const mathCalc = compose(half, square, double); // all functions for compose are executed in reverse order
-  const mathCalcPipe = pipe(double, square, half); // pipe executes in direct order
+  // const divide = (n1, n2) => n1 / n2;
+  //curring
+  const divideCurr = (n2) => {
+    return function (n1) {
+      return n1 / n2;
+    };
+  };
+  // const mathCalc = compose(half, square, double); // all functions for compose are executed in reverse order
+  const mathCalcPipe = pipe(double, square, half, divideCurr(3)); // pipe executes in direct order
 
   return <h1>{mathCalcPipe(x)}</h1>;
 };
