543027d41422085682d5521d5075425a470d9d7d
Olga Melnikova <omelnikova@accesssoftek.com>
2022-03-09 10:35:09 +0400

    create store

--- src/index.js
+++ src/index.js
@@ -1,24 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
-import { compose, pipe } from "lodash/fp";
 
+function createStore(initialState) {
+  let state = initialState;
+  function getState() {
+    return state;
+  }
+  return { getState };
+}
+
+const store = createStore([{ id: 1, description: "Task1", completed: false }]);
 const App = (params) => {
-  const x = 2;
-
-  const double = (number) => number * 2;
-  const square = (number) => number * number;
-  const half = (number) => number / 2;
-  // const divide = (n1, n2) => n1 / n2;
-  //curring
-  const divideCurr = (n2) => {
-    return function (n1) {
-      return n1 / n2;
-    };
-  };
-  // const mathCalc = compose(half, square, double); // all functions for compose are executed in reverse order
-  const mathCalcPipe = pipe(double, square, half, divideCurr(3)); // pipe executes in direct order
-
-  return <h1>{mathCalcPipe(x)}</h1>;
+  console.log(store.getState());
+  return <h1>app</h1>;
 };
 
 ReactDOM.render(
