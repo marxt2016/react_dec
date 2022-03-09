268b81cad401e931f93427896afa7c66e11e2a92
Olga Melnikova <omelnikova@accesssoftek.com>
2022-03-09 13:39:27 +0400

    dispatch

--- src/index.js
+++ src/index.js
@@ -6,13 +6,30 @@ function createStore(initialState) {
   function getState() {
     return state;
   }
-  return { getState };
+  function dispatch(action) {
+    console.log(action);
+    if (action.type === "task/completed") {
+      const newArray = [...state];
+      const elementIndex = newArray.findIndex((el) => el.id === action.payload.id);
+      newArray[elementIndex].completed = true;
+      state = newArray;
+      console.log(state);
+    }
+  }
+
+  return { getState, dispatch };
 }
 
 const store = createStore([{ id: 1, description: "Task1", completed: false }]);
 const App = (params) => {
   console.log(store.getState());
-  return <h1>app</h1>;
+
+  return (
+    <>
+      <button onClick={() => store.dispatch({ type: "task/completed", payload: { id: 1 } })}>Complete task</button>
+      <h1>app</h1>
+    </>
+  );
 };
 
 ReactDOM.render(
