2294ff260f57528c808a1e73cd4a6c8bac4a869d
Olga Melnikova <omelnikova@accesssoftek.com>
2022-03-14 10:05:11 +0400

    refactor create store and update actions

--- src/index.js
+++ src/index.js
@@ -1,43 +1,53 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import * as actions from "./store/actionTypes";
+import { createStore } from "./store/createStore";
+import { taskReducer } from "./store/taskReducer";
 
-function taskReducer(state, action) {
-  switch (action.type) {
-    case "task/completed":
-      const newArray = [...state];
-      const elementIndex = newArray.findIndex((el) => el.id === action.payload.id);
-      newArray[elementIndex].completed = true;
-      return newArray;
+// function taskReducer(state, action) {
+//   switch (action.type) {
+//     // case "task/completed":
+//     //   const newArray = [...state];
+//     //   const elementIndex = newArray.findIndex((el) => el.id === action.payload.id);
+//     //   newArray[elementIndex].completed = true;
+//     //   return newArray;
+//     case "task/updated": {
+//       const newArray = [...state];
+//       const elementIndex = newArray.findIndex((el) => el.id === action.payload.id);
+//       newArray[elementIndex] = { ...newArray[elementIndex], ...action.payload };
+//       return newArray;
+//     }
+//     default:
+//       break;
+//   }
+// }
 
-    default:
-      break;
-  }
-}
+// function createStore(reducer, initialState) {
+//   let state = initialState;
+//   let listeners = [];
+//   function getState() {
+//     return state;
+//   }
+//   function dispatch(action) {
+//     state = reducer(state, action);
+//     for (let i = 0; i < listeners.length; i++) {
+//       const listener = listeners[i];
+//       listener();
+//     }
+//   }
+//   function subscribe(listener) {
+//     listeners.push(listener);
+//   }
 
-function createStore(reducer, initialState) {
-  let state = initialState;
-  let listeners = [];
-  function getState() {
-    return state;
-  }
-  function dispatch(action) {
-    state = reducer(state, action);
-    for (let i = 0; i < listeners.length; i++) {
-      const listener = listeners[i];
-      listener();
-    }
-  }
-  function subscribe(listener) {
-    listeners.push(listener);
-  }
+//   return { getState, dispatch, subscribe };
+// }
 
-  return { getState, dispatch, subscribe };
-}
+const initialState = [
+  { id: 1, title: "Task1", completed: false },
+  { id: 2, title: "Task2", completed: false },
+];
 
-const store = createStore(taskReducer, [
-  { id: 1, description: "Task1", completed: false },
-  { id: 2, description: "Task2", completed: false },
-]);
+const store = createStore(taskReducer, initialState);
 const App = (params) => {
   const [state, setState] = useState(store.getState());
   useEffect(() => {
@@ -47,7 +57,10 @@ const App = (params) => {
   }, []);
 
   const completeTask = (taskId) => {
-    store.dispatch({ type: "task/completed", payload: { id: taskId } });
+    store.dispatch({ type: actions.taskUpdated, payload: { id: taskId, completed: true } });
+  };
+  const changeTitlle = (taskId) => {
+    store.dispatch({ type: actions.taskUpdated, payload: { id: taskId, title: `New title for ${taskId}` } });
   };
 
   return (
@@ -56,9 +69,10 @@ const App = (params) => {
       <ul>
         {state.map((el) => (
           <li key={el.id}>
-            <p>{el.description}</p>
+            <p>{el.title}</p>
             <p>{`Completed: ${el.completed}`}</p>
             <button onClick={() => completeTask(el.id)}>Complete task</button>
+            <button onClick={() => changeTitlle(el.id)}>Change title</button>
             <hr />
           </li>
         ))}
