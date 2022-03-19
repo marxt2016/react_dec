903adc9639f5101a3abd3edd839f0c3c0a9b8748
Olga Melnikova <omelnikova@accesssoftek.com>
2022-03-19 09:05:29 +0400

    useDispatch()

--- src/index.js
+++ src/index.js
@@ -1,30 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom";
 import { titleChanged, taskDeleted, completeTask, getTasks } from "./store/task";
 import configureStore from "./store/store";
-import { Provider, useSelector } from "react-redux";
+import { Provider, useSelector, useDispatch } from "react-redux";
 
 const store = configureStore();
 
 const App = (params) => {
-  // const [state, setState] = useState(store.getState());
   const state = useSelector((state) => state);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    store.dispatch(getTasks());
-    // store.subscribe(() => {
-    //   setState(store.getState());
-    // });
+    dispatch(getTasks());
   }, []);
 
   const changeTitlle = (taskId) => {
-    store.dispatch(() => {
-      store.dispatch(titleChanged(taskId));
-    });
+    dispatch(titleChanged(taskId));
   };
 
   const deleteTask = (taskId) => {
-    store.dispatch(taskDeleted(taskId));
+    dispatch(taskDeleted(taskId));
   };
 
   return (
@@ -35,7 +30,7 @@ const App = (params) => {
           <li key={el.id}>
             <p>{el.title}</p>
             <p>{`Completed: ${el.completed}`}</p>
-            <button onClick={() => store.dispatch(completeTask(el.id))}>Complete task</button>
+            <button onClick={() => dispatch(completeTask(el.id))}>Complete task</button>
             <button onClick={() => changeTitlle(el.id)}>Change title</button>
             <button onClick={() => deleteTask(el.id)}>Delete task</button>
             <hr />
